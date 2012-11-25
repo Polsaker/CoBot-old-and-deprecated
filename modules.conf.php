7f758fa0b24483ee0a9ee39e9199233b70fb2560
Ramiro Bou <bouramiro@gmail.com>
2012-11-25 17:41:58 -0300

    Agregada nota de por que m_iplocator esta comentado

--- modules.conf.php
+++ modules.conf.php
@@ -5,6 +5,7 @@ $ircbot->load("m_say.php"); // Agrega el comando say
 $ircbot->load("m_google.php"); // Agrega el comando google
 $ircbot->load("m_authadd.php"); // Permite el registro de nuevos usuarios
 $ircbot->load("m_ping.php"); // agrega el comando ping
+//comentado por defecto debido a que requiere la descarga de archivos externos (vease mas en cobot.tk)
 #include("geoloc/geoipcity.inc");include("geoloc/geoipregionvars.php");include("geoloc/timezone.php"); //requerido por m_iplocator
 #$ircbot->load("m_iplocator.php"); //permite geolocalizar IPs con GeoIP [requiere geoipcity.inc, geoipregionvars.php y timezone.php]
 $ircbot->load("m_nick.php"); //agrega el comando nick.
@@ -20,6 +21,7 @@ $ircbot->load("m_hash.php");
 $ircbot->load("m_modules.php");
 $ircbot->load("m_ignore.php");
 $ircbot->load("m_booksearch.php");
+$ircbot->load("m_short.php");
 
 
 ?>
