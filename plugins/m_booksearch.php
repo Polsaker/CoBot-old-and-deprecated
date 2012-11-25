7e0571a88a6b508ac71d1637faf4eb44e33dc159
Ramiro Bou <bouramiro@gmail.com>
2012-11-25 02:13:11 -0300

    Ahora no se muestran los valores que no existen

--- plugins/m_booksearch.php
+++ plugins/m_booksearch.php
@@ -26,7 +26,11 @@ class ee111t1t1172{
 			if($jao->totalItems==0){$resp="No se encontraron resultados..";}else{
 				$i=0;
 				while(($i<3) && (@$jao->items[$i])){
-					@$resp="".$jao->items[$i]->volumeInfo->title.", Autor: ".$jao->items[$i]->volumeInfo->authors[0].", ".$jao->items[$i]->volumeInfo->pageCount." páginas. ISBN-10: ".$jao->items[$i]->volumeInfo->industryIdentifiers[$i]->identifier.", ISBN-13 ".$jao->items[$i]->volumeInfo->industryIdentifiers[1]->identifier.".";
+					@$resp="".$jao->items[$i]->volumeInfo->title."";
+					if(@$jao->items[$i]->volumeInfo->authors[0]){$resp.=", Autor: ".$jao->items[$i]->volumeInfo->authors[0]."";}
+					if($jao->items[$i]->volumeInfo->pageCount){$resp.=", ".$jao->items[$i]->volumeInfo->pageCount." páginas.";}
+					if($jao->items[$i]->volumeInfo->industryIdentifiers[$i]->identifier){$resp.=" ISBN-10: ".$jao->items[$i]->volumeInfo->industryIdentifiers[$i]->identifier."";}
+					if($jao->items[$i]->volumeInfo->industryIdentifiers[1]->identifier){$resp.=", ISBN-13 ".$jao->items[$i]->volumeInfo->industryIdentifiers[1]->identifier.".";}
 					$irc->SendCommand("PRIVMSG ".$channel." :".$resp);
 					$i++;
 				}
