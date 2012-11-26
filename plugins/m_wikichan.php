dc61676b1d58a9b79fee58e3df8956de9f78c645
Ramiro Bou <bouramiro@gmail.com>
2012-11-26 00:22:19 -0300

    Ahora usa json!

--- plugins/m_wikichan.php
+++ plugins/m_wikichan.php
@@ -8,7 +8,6 @@ class ee111t1t1172{
 		$irc->addcmd($this, 'info', 'wikichn');	
 		$irc->addcmd($this, 'nickassoc', 'wikichn', array("wikinick","na"));	
 		$irc->addcmd($this, 'wikisearch', 'wikichn');	
-		$irc->addcmd($this, 'lrev', 'wikichn');	
 		$irc->addcmd($this, 'preview', 'wikichn',array("lpreview"));	
 		$irc->addcmd($this, 'contribs', 'wikichn',array("contribuciones", "aportes","contrib"));
 		$irc->addcmd($this, 'chwadd', 'wikichn');
@@ -16,7 +15,6 @@ class ee111t1t1172{
 		$this->help['info']='Proporciona información de un usuario de una wiki. Sintaxis: info <usuario>';
 		$this->help['nickassoc']='Asocia un nick de IRC con un usuario de una wiki. Sintaxis nickassoc <usuario>';
 		$this->help['wikisearch']='Busca un articulo en una wiki.';
-		$this->help['lrev']='Muestra la información de la última revision de un articulo.';
 		$this->help['preview']='Muestra una vista previa de hasta 250 caracteres de un artículo.';
 		$this->help['lpreview']='Muestra una vista previa de hasta 440 caracteres de un artículo.';
 		$this->help['contribs']='Muestra el número de contribuciones de un usuario en una determinada cantidad de dias. Sintaxis: contribs <dias> <usuario>.';
@@ -61,22 +59,36 @@ class ee111t1t1172{
 		$ts=trim($ts);
 		
 
-		$jsn = file_get_contents("http://".$this->chans[$channel]."/api.php?action=query&list=users&ususers=".urlencode(str_replace(" ", "_",$ts))."&format=xml&usprop=blockinfo|editcount|registration|gender|groups");		
+		$jsn = file_get_contents("http://".$this->chans[$channel]."/api.php?action=query&list=users&ususers=".urlencode(str_replace(" ", "_",$ts))."&format=json&usprop=blockinfo|editcount|registration|gender|groups");		
 		//$result = xml2array($xml);
 		if($jsn==""){$irc->SendCommand("PRIVMSG ".$channel." :Error interno \002001\002. Notifique al administrador.");return 0;}
 		$result=json_decode($jsn);
 				
-		if(@!isset($result['api']['query']['users']['user']['attr']['missing'])){
-			$registration=date("d/m/Y H:i",strtotime(substr($result['api']['query']['users']['user']['attr']['registration'],0,strlen($result['api']['query']['users']['user']['attr']['registration'])-1)));
-			$resp="Usuario ".$result['api']['query']['users']['user']['attr']['name']." - Registrado: ". $registration. " - Ediciones: ".$result['api']['query']['users']['user']['attr']['editcount'] ." - ";
+		if(@!isset($result->query->users[0]->missing)){
+			if($result->query->users[0]->registration){
+				$registration=date("d/m/Y H:i",strtotime(substr($result->query->users[0]->registration,0,strlen($result->query->users[0]->registration)-1)));
+			}else{$registration="04N/D";}
+			/*$xml = file_get_contents("http://".$this->chans[$channel]."/api.php?action=userdailycontribs&user=".urlencode(str_replace(" ", "_",$ts))."&daysago=".$param[1]."&format=json");		
+		$result = xml2array($xml);
+		if(!$xml){$irc->SendCommand("PRIVMSG ".$channel." :\00305Error\003 al procesar el comando");return 0;}
+		$irc->SendCommand("PRIVMSG ".$channel." :Contribuciones del usuario \002".$ts."\002 en los últimos ".$param[1]." dia(s): \002".$result['api']['userdailycontribs']['attr']['timeFrameEdits']."\002 de \002".$result['api']['userdailycontribs']['attr']['totalEdits']."\002 contribuciones en total.");
+*/
+			$w2 = file_get_contents("http://".$this->chans[$channel]."/api.php?action=userdailycontribs&user=".urlencode(str_replace(" ", "_",$ts))."&daysago=7&format=json"); $r2=json_decode($w2);
+			$w3 = file_get_contents("http://".$this->chans[$channel]."/api.php?action=userdailycontribs&user=".urlencode(str_replace(" ", "_",$ts))."&daysago=30&format=json"); $r3=json_decode($w3);
+			$w4 = file_get_contents("http://".$this->chans[$channel]."/api.php?action=userdailycontribs&user=".urlencode(str_replace(" ", "_",$ts))."&daysago=180&format=json"); $r4=json_decode($w4);
+			
+			
+			$resp="Usuario ".$result->query->users[0]->name." - Registrado: ". $registration. " - Ediciones: ".$result->query->users[0]->editcount ." (Semana: ".$r2->userdailycontribs->timeFrameEdits.", Mes: ".$r3->userdailycontribs->timeFrameEdits.", ~6 meses:".$r4->userdailycontribs->timeFrameEdits.") - ";
 			$i=0;
 			$resp.="Grupos: ";
-			while(@isset($result['api']['query']['users']['user']['groups']['g'][$i+1])){
+			while(@isset($result->query->users[0]->groups[$i+1])){
 				$i++;
-				$resp.=$result['api']['query']['users']['user']['groups']['g'][$i]['value']. ", ";
+				if($result->query->users[0]->groups[$i]!="*"){
+					$resp.=$result->query->users[0]->groups[$i]. ", ";
+				}
 			}
 			$resp=substr($resp,0,strlen($resp)-2);
-			switch(@$result['api']['query']['users']['user']['attr']['gender']){
+			switch(@$result->query->users[0]->gender){
 				case 'male':
 					$resp.=" - Sexo: 12Masculino ♂";
 					break;
@@ -84,11 +96,11 @@ class ee111t1t1172{
 					$resp.=" - Sexo: 13Femenino ♀";
 					break;
 				case 'unknown':
-					$resp.=" - Sexo: (?)";
+					$resp.=" - Sexo: 04N/D";
 					break;
 			}
-			if(@$result['api']['query']['users']['user']['attr']['blockid']){
-				$resp.=" - 05Usuario bloqueado por ".$result['api']['query']['users']['user']['attr']['blockedby'].": ".$result['api']['query']['users']['user']['attr']['blockreason'];
+			if(@isset($result->query->users[0]->blockid)){
+				$resp.=" - 05Usuario bloqueado por ".$result->query->users[0]->blockedby.": ".$result->query->users[0]->blockreason;
 			}
 		}else{$resp="Usuario inexistente.";}
 		$irc->SendCommand("PRIVMSG ".$channel." :".$resp);
@@ -113,29 +125,16 @@ class ee111t1t1172{
 			$i++;
 		}
 		$ts=trim($ts);
-		$xml = file_get_contents("http://".$this->chans[$channel]."/api.php?action=query&list=search&srsearch=".urlencode(str_replace(" ", "_",$ts))."&format=xml&text=title&srlimit=5&srprop=snippet");		
-		$result = xml2array($xml);
+		$xml = file_get_contents("http://".$this->chans[$channel]."/api.php?action=query&list=search&srsearch=".urlencode(str_replace(" ", "_",$ts))."&format=json&text=title&srlimit=5&srprop=snippet");		
+		$result = json_decode($xml);
 		$i=0;$r="";
-		while(@isset($result['api']['query']['search']['p'][$i]['attr']['title'])){
-			//echo $result['api']['query']['search']['p'][$i]['attr']['title'];
-			$r.="\002\037".$result['api']['query']['search']['p'][$i]['attr']['title']. "\037\002 \00310http://".$this->chans[$channel]."/?title=".urlencode(str_replace(" ", "_",$result['api']['query']['search']['p'][$i]['attr']['title']))."\003 - ";$i++;
+		while(@isset($result->query->search[$i]->title)){
+			$r.="\002\037".$result->query->search[$i]->title. "\037\002 \00310http://".$this->chans[$channel]."/?title=".urlencode(str_replace(" ", "_",$result->query->search[$i]->title))."\003 - ";$i++;
 		}
 		$r=substr($r,0,strlen($r)-3);
 		$irc->SendCommand("PRIVMSG ".$channel." :Resultados: ".$r);
 	}
-	public function lrev(&$irc,$msg,$channel,$param,$who){
-		if(!@isset($this->chans[strtolower($channel)])){$irc->SendCommand("PRIVMSG $channel :\00305Error\003: Esta función no esta habilitada aqui!");return 0;}
-		$i=1;
-		$ts="";
-		while(@isset($param[$i])){
-			$ts.=$param[$i]. " ";
-			$i++;
-		}
-		$ts=trim($ts);
-		$xml = file_get_contents("http://".$this->chans[$channel]."/api.php?action=query&format=xml&prop=revisions&titles=".urlencode(str_replace(" ", "_",$ts))."&rvlimit=5&rvprop=user|comment");		
-		$result = xml2array($xml);
-		$irc->SendCommand("PRIVMSG ".$channel." :Última edicion del articulo por \002".$result['api']['query']['pages']['page']['revisions']['rev'][0]['attr']['user']."\002 ".($result['api']['query']['pages']['page']['revisions']['rev'][0]['attr']['comment']!=""?"Comentario: \002".$result['api']['query']['pages']['page']['revisions']['rev'][0]['attr']['comment']:""));
-	}
+
 	public function preview(&$irc,$msg,$channel,$param,$who){
 		if(!@isset($this->chans[strtolower($channel)])){$irc->SendCommand("PRIVMSG $channel :\00305Error\003: Esta función no esta habilitada aqui!");return 0;}
 		$i=1;
@@ -145,21 +144,22 @@ class ee111t1t1172{
 			$i++;
 		}
 		$ts=trim($ts);
-		$xml = file_get_contents("http://".$this->chans[$channel]."/api.php?action=query&prop=extracts&exchars=440&titles=".urlencode(str_replace(" ", "_",$ts))."&format=xml");		
-		$result = xml2array($xml);
-		if(@isset($result['api']['query']['pages']['page']['attr']['missing'])){$irc->SendCommand("PRIVMSG ".$channel.' :04Error: El artículo no existe.');return 0;}
-		$rs=$result['api']['query']['pages']['page']['extract']['value'];
-		if(preg_match('#^REDIRECCIÓN (.+)$#',strip_tags($rs), $matches)){
+		$xml =  file_get_contents("http://".$this->chans[$channel]."/api.php?action=query&prop=extracts&exchars=440&titles=".urlencode(str_replace(" ", "_",$ts))."&format=json");		
+		$result = json_decode($xml);
+		foreach($result->query->pages as $key => $val){$pageid=$key;}
+		if(@isset($result->query->pages->$pageid->missing)){$irc->SendCommand("PRIVMSG ".$channel.' :04Error: El artículo no existe.');return 0;}
+		$rs=$result->query->pages->$pageid->extract;
+		if(preg_match('#^.*REDIRECCIÓN (.+)$#',strip_tags($rs), $matches)){
 			$this->preview($irc,$msg,$channel,array("",$matches[1]),$who);
 			return 0;
 		}
-		if(preg_match('#^REDIRECT (.+)$#',strip_tags($rs), $matches)){
+		if(preg_match('#^.*REDIRECT (.+)$#',strip_tags($rs), $matches)){
 			$this->preview($irc,$msg,$channel,array("",$matches[1]),$who);
 			return 0;
 		}
 		$rs=$this->wformat($rs);
 		
-		$irc->SendCommand("PRIVMSG ".$channel.' :"'.$rs.'"');
+		$irc->SendCommand("PRIVMSG ".$channel.' :"'.mb_convert_encoding($rs,$irc->conf['conn']['charset']).'"');
 		$irc->SendCommand("PRIVMSG ".$channel.' :10http://'.$this->chans[$channel].'/index.php?title='.urlencode(str_replace(" ", "_",$ts)));
 	}
 	private function wformat($text){
@@ -170,8 +170,8 @@ class ee111t1t1172{
 		$text=str_replace("<u>","\037",$text);
 		$text=str_replace("</u>","\037",$text);
 		$text=str_replace("\n","||",$text);
-		$text=strip_tags($text);
-		$text=html_entity_decode($text);
+		$text=mb_convert_encoding(strip_tags($text),$irc->conf['conn']['charset']);
+		$text=mb_convert_encoding(html_entity_decode($text),$irc->conf['conn']['charset']);
 		return $text;
 	}
 	public function contribs(&$irc,$msg,$channel,$param,$who){
@@ -203,10 +203,10 @@ class ee111t1t1172{
 		}
 		mysql_close($myconn);
 		$ts=trim($ts);
-		$xml = file_get_contents("http://".$this->chans[$channel]."/api.php?action=userdailycontribs&user=".urlencode(str_replace(" ", "_",$ts))."&daysago=".$param[1]."&format=xml");		
-		$result = xml2array($xml);
+		$xml = file_get_contents("http://".$this->chans[$channel]."/api.php?action=userdailycontribs&user=".urlencode(str_replace(" ", "_",$ts))."&daysago=".$param[1]."&format=json");		
+		$result = json_decode($xml);
 		if(!$xml){$irc->SendCommand("PRIVMSG ".$channel." :\00305Error\003 al procesar el comando");return 0;}
-		$irc->SendCommand("PRIVMSG ".$channel." :Contribuciones del usuario \002".$ts."\002 en los últimos ".$param[1]." dia(s): \002".$result['api']['userdailycontribs']['attr']['timeFrameEdits']."\002 de \002".$result['api']['userdailycontribs']['attr']['totalEdits']."\002 contribuciones en total.");
+		$irc->SendCommand("PRIVMSG ".$channel." :Contribuciones del usuario \002".$ts."\002 en los últimos ".$param[1]." dia(s): \002".$result->userdailycontribs->timeFrameEdits."\002 de \002".$result->userdailycontribs->totalEdits."\002 contribuciones en total.");
 	}
 	public function chwadd(&$irc,$msg,$channel,$param,$who){
 		if(!$irc->checkauth($who,5)==1){$irc->SendCommand("PRIVMSG $channel :\00305Error\003: No posees los privilegios suficientes para usar este comando.");return 0;}
