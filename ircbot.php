6c10f97db09337dd30d3982fe8b166705e589744
Ramiro Bou <bouramiro@gmail.com>
2012-11-30 23:21:15 -0300

    Cambio de versión

--- ircbot.php
+++ ircbot.php
@@ -3,7 +3,7 @@
  * Hecho por Ramiro Bou bajo la licencia CC-By-NC-SA
  * http://creativecommons.org/licenses/by-nc-sa/3.0/
  */ 
-	define("VER", "0.2.0.0.3");
+	define("VER", "0.2.0.1");
 	date_default_timezone_set('UTC');
 	require("config.php");
 	require("ircbot.class.php");
