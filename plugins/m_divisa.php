40ab41463712fa4d5b1f3d9e8c79f8ec9e87c8bd
Ramiro Bou <bouramiro@gmail.com>
2012-12-02 15:32:26 -0300

    Ahora funcional

--- plugins/m_divisa.php
+++ plugins/m_divisa.php
@@ -14,19 +14,10 @@ class ee111t1t1172{
 
 	public function conv(&$irc,$msg,$channel,$param,$who){
 		if(!isset($param[3])){$irc->SendPriv($channel,"03Error: Faltan parámetros");return 0;}
-		$div=$this->conversor_divisas($param[1],$param[2],$param[3]);
-		$irc->SendPriv($channel,"Convirtiendo de $param[1] a $param[2]: $div");
-	}
-	private function conversor_divisas($divisa_origen, $divisa_destino, $cantidad) {
-		$cantidad = urlencode($cantidad);
-		$divisa_origen = urlencode($divisa_origen);
-		$divisa_destino = urlencode($divisa_destino);
-		$url = "http://www.google.com/ig/calculator?hl=en&q=$cantidad$divisa_origen=?$divisa_destino";
-		$rawdata = file_get_contents($url);
-		$data = explode('"', $rawdata);
-		$data = explode(' ', $data['3']);
-		$var = $data['0'];
-		return round($var,2);
+		$f=file_get_contents("http://rate-exchange.appspot.com/currency?from=$param[1]&to=$param[2]&q=$param[3]");
+		$js=json_decode($f);
+		$div=$js->v;
+		$irc->SendPriv($channel,"Convirtiendo $param[1] a $param[2]: $div");
 	}
 }
 ?>
