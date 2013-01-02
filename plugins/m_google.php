faa2e37e39892a527c0b9f9bf09cb34591711b53
Ramiro Bou <bouramiro@gmail.com>
2013-01-02 20:08:01 -0300

    Actualizado para el uso de la nueva función del core

--- plugins/m_google.php
+++ plugins/m_google.php
@@ -14,13 +14,7 @@ class ee111t1t1172{
 
 		public function google(&$irc,$msg,$channel,$param,$who)
 		{
-			$i=1;
-			$ts="";
-			while(@isset($param[$i])){
-				$ts.=$param[$i]. " ";
-				$i++;
-			}
-			$ts=substr($ts,0,strlen($ts)-1);
+			$irc->jparam($param,1);
 			$gap=file_get_contents("https://www.googleapis.com/customsearch/v1?num=3&key=".$irc->conf['m_google']['api_key']."&cx=001206920739550302428:fozo2qblwzc&q=".urlencode($ts)."&alt=json");
 			$jao=json_decode($gap);
 			$resp="Resultados de la búsqueda en Google de \"".$ts."\": ".$jao->items[0]->title." 10".$jao->items[0]->link." ".$jao->items[1]->title." 10".$jao->items[1]->link." ".$jao->items[2]->title." 10".$jao->items[2]->link."";
