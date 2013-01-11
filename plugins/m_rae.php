10e5a473ed2f04f0b2fbff7ce3601387db08235b
Ramiro Bou <bouramiro@gmail.com>
2013-01-11 20:39:05 -0300

    idem

--- plugins/m_rae.php
+++ plugins/m_rae.php
@@ -12,7 +12,7 @@ class ee111t1t1172{
 	public function rae(&$irc,$msg,$channel,$param,$who){
 		if(!isset($param[1])){$irc->SendPriv($channel,"03Error: Faltan parámetros");return 0;}
 		$f=file_get_contents("http://rae-quel.appspot.com/json?query=".strtolower($param[1]));
-		$js=json_decode($f);$div="Definiciones encontradas de la palabra \"".strtolower($param[1])."\": ";
+		$js=json_decode($f);$div="Definiciones encontradas de la palabra \"".strtolower($param[1])."\": ";
 		foreach($js as $key=>$val){
 			$div.="\"$val\", ";
 		}
