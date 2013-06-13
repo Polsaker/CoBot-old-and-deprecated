b61272031d7466f430978230a6920f7df3fab251
Ramiro Bou <bouramiro@gmail.com>
2013-06-13 19:58:58 -0300

    Pig-Pog

--- plugins/m_ping.php
+++ plugins/m_ping.php
@@ -10,11 +10,14 @@ class ee111t1t1172{
 	public function __construct(&$irc){	
 		$irc->addcmd($this, 'ping', 'ping');	
 		$irc->addcmd($this, 'pong', 'pong');	
+		$irc->addcmd($this, 'pig', 'pig');	
 		$this->help['ping']='Responde con PONG';
 		$this->help['pong']='Responde con PING';
+		$this->help['pig']='Responde con POG';
 	}
 
 	public function ping(&$irc,$msg,$channel,$param,$who){ $irc->SendCommand("PRIVMSG ".$channel." :PONG");}
 	public function pong(&$irc,$msg,$channel,$param,$who){ $irc->SendCommand("PRIVMSG ".$channel." :PING");}
+	public function pig(&$irc,$msg,$channel,$param,$who){ $irc->SendCommand("PRIVMSG ".$channel." :POG");}
 }
 ?>
