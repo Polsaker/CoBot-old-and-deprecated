360e8861c07588549c8e401f2bfdc41109a0a580
Ramiro Bou <bouramiro@gmail.com>
2013-09-17 01:57:14 -0300

    woops

--- ircbot.class.php
+++ ircbot.class.php
@@ -442,7 +442,7 @@ class IRCBot{
 							while(@$rowx=$rsx->fetch_array()){
 								if(preg_match("#".$rowx['host']."#",$who,$m)){break;}
 							}
-						$myconn->close();*/
+						$myconn->close();*/g
 						$this->procom($msg,$who,$channel);
 						break;
 				}
