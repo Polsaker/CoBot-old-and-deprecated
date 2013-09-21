e85878bfeb1402d526900c4c63318e94e4a86935
Ramiro Bou <bouramiro@gmail.com>
2013-09-21 20:22:18 -0300

    Creada una carpeta para librerias

--- cobot.php
+++ cobot.php
@@ -3,7 +3,7 @@
 	define("VER", "1.0 Alpha");
 	date_default_timezone_set('UTC');
 	require("config.php");
-	require("mustached-ironman/SmartIRC.php");
+	require("lib/SmartIRC/SmartIRC.php");
 	require("cobot.core.php");
 	
 	$cobot = new CoBot($conf);
