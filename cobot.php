a4c6a358856acdffa4e716add9fc4b9e69dc7a15
Ramiro Bou <bouramiro@gmail.com>
2013-09-23 02:14:38 -0300

    Incluyendo a idiorm

--- cobot.php
+++ cobot.php
@@ -3,6 +3,7 @@
 	define("VER", "1.0 Alpha");
 	date_default_timezone_set('UTC');
 	require("config.php");
+	require("lib/idiorm/idiorm.php");
 	require("lib/SmartIRC/SmartIRC.php");
 	require("cobot.core.php");
 	
