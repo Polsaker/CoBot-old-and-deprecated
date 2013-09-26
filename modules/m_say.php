9440596d124c5db7534303dacc603744b22d57cb
Alumno <alumno@alumno-laptop.(none)>
2013-09-26 13:41:57 -0300

    Agregando modulo m_say

---
+++ modules/m_say.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * @name: Say
+ * @desc: Hace que el bot hable
+ * @ver: 1.0
+ * @author: MRX
+ * @id: say
+ * @key: quemierdahacesmirandoesto
+ *
+ */
+
+class quemierdahacesmirandoesto{
+	public function __construct(&$core){
+		$core->registerCommand("say", "say", "Hace que el bot hable. Sintaxis: say <canal> <mensaje>", 1);
+	}
+	
+	public function say(&$irc, $data, &$core){
+		$ts = $core->jparam, $data->messageex,2);
+		$irc->message(SMARTIRC_TYPE_CHANNEL, $data->messageex[1], $ts);
+	}
+}
