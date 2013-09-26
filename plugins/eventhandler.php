39eac8f130971f3b643820180674ceb60b914276
Ramiro Bou <bouramiro@gmail.com>
2013-09-25 21:22:43 -0300

    Oooops... Me equivoqué de branch

--- plugins/eventhandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
-TODO:
-Clase extendiendo Net_SmartIRC. Debe contener todas las funciones de los eventhandlers de SmartIRC
-Cada eventhandler debe llamar a una funcion en el core que lo procesara y llamara a las funciones necesarias.
-
-
-*/
