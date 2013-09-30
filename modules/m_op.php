481e58f1efe507c6c615ee3d39d12cc8d10b80b6
Ramiro Bou <bouramiro@gmail.com>
2013-09-30 00:22:47 -0300

    Finalizando m_op Falta algo de esfuerzo, no?

--- modules/m_op.php
+++ modules/m_op.php
@@ -18,6 +18,7 @@ class ghasts{
 		$core->registerCommand("devoice", "op", "Quita voz en un canal. Sintaxis: devoice [canal] [nick]", 5);
 		$core->registerCommand("kick", "op", "Kickea a una persona en un canal. Sintaxis: kick [canal] [nick]", 5);
 		$core->registerCommand("kickban", "op", "Banea a alguien en un canal. Sintaxis: kickban [canal] [nick]", 5);
+		$core->registerCommand("unban", "op", "Desanea a alguien en un canal. Sintaxis: unban [canal] [nick]", 5);
 		$core->irc->setChannelSyncing(true);
 		$core->registerMessageHandler('352', "op", "whorecv");
 	}
@@ -25,10 +26,29 @@ class ghasts{
 	public function whorecv(&$irc, $data, $core){
 		print_r($data);
 		if($data->messageex[1] == $this->searchfor){
-			$irc->ban($this->searchforchan, "*!*@".$data->rawmessageex[5]);
-			$irc->kick($this->searchforchan, $this->searchfor);
+			if($this->ban==true){
+				$irc->ban($this->searchforchan, "*!*@".$data->rawmessageex[5]);
+				$irc->kick($this->searchforchan, $this->searchfor);
+			}else{
+				$irc->unban($this->searchforchan, "*!*@".$data->rawmessageex[5]);
+			}
 			$this->searchfor = "";
 			$this->searchforchan = "";
+		}
+	}
+	
+	public function unban(&$irc, $data, &$core){
+		if(substr($data->messageex[1],0,1)=="#"){$chan=$data->messageex[1];}else{$chan=$data->channel;}
+		if(!isset($data->messageex[1]) || (substr($data->messageex[1],0,1)=="#" && !isset($data->messageex[2]))){$user=$data->nick;}elseif(substr($data->messageex[1],0,1)=="#"){$user=$data->messageex[2];}else{$user=$data->messageex[1];}
+		if($irc->isOpped($chan)){
+			if(preg_match("·.+!.+@.*·", $user)){
+				$irc->unban($chan,$user);
+			}else{
+				$this->ban = false;
+				$this->searchfor = $user;
+				$this->searchforchan = $chan;
+				$irc->who($user);
+			}
 		}
 	}
 	
@@ -36,9 +56,14 @@ class ghasts{
 		if(substr($data->messageex[1],0,1)=="#"){$chan=$data->messageex[1];}else{$chan=$data->channel;}
 		if(!isset($data->messageex[1]) || (substr($data->messageex[1],0,1)=="#" && !isset($data->messageex[2]))){$user=$data->nick;}elseif(substr($data->messageex[1],0,1)=="#"){$user=$data->messageex[2];}else{$user=$data->messageex[1];}
 		if($irc->isOpped($chan)){
-			$this->searchfor = $user;
-			$this->searchforchan = $chan;
-			$irc->who($user);
+			if(preg_match("·.+!.+@.*·", $user)){
+				$irc->ban($chan,$user);
+			}else{
+				$this->ban = true;
+				$this->searchfor = $user;
+				$this->searchforchan = $chan;
+				$irc->who($user);
+			}
 		}
 	}
 	
