29d7e14a3fe0cc71b0cb03145a38bda139cb26d1
Ramiro Bou <bouramiro@gmail.com>
2013-09-29 22:02:33 -0300

    Mejorando m_op Pendiente: unban y autokick

--- modules/m_op.php
+++ modules/m_op.php
@@ -9,16 +9,75 @@
  *
  */
 class ghasts{
+	private $searchfor;
+	private $searchforchan;
 	public function __construct($core){
 		$core->registerCommand("op", "op", "Da OP en un canal. Sintaxis: op [canal] [nick]", 5);
 		$core->registerCommand("deop", "op", "Quita OP en un canal. Sintaxis: deop [canal] [nick]", 5);
-		$core->irc->setChannelSynching(true);
+		$core->registerCommand("voice", "op", "Da voz en un canal. Sintaxis: voice [canal] [nick]", 5);
+		$core->registerCommand("devoice", "op", "Quita voz en un canal. Sintaxis: devoice [canal] [nick]", 5);
+		$core->registerCommand("kick", "op", "Kickea a una persona en un canal. Sintaxis: kick [canal] [nick]", 5);
+		$core->registerCommand("kickban", "op", "Banea a alguien en un canal. Sintaxis: kickban [canal] [nick]", 5);
+		$core->irc->setChannelSyncing(true);
+		$core->registerMessageHandler('352', "op", "whorecv");
+	}
+	
+	public function whorecv(&$irc, $data, $core){
+		print_r($data);
+		if($data->messageex[1] == $this->searchfor){
+			$irc->ban($this->searchforchan, "*!*@".$data->rawmessageex[5]);
+			$irc->kick($this->searchforchan, $this->searchfor);
+			$this->searchfor = "";
+			$this->searchforchan = "";
+		}
+	}
+	
+	public function kickban(&$irc, $data, &$core){
+		if(substr($data->messageex[1],0,1)=="#"){$chan=$data->messageex[1];}else{$chan=$data->channel;}
+		if(!isset($data->messageex[1]) || (substr($data->messageex[1],0,1)=="#" && !isset($data->messageex[2]))){$user=$data->nick;}elseif(substr($data->messageex[1],0,1)=="#"){$user=$data->messageex[2];}else{$user=$data->messageex[1];}
+		if($irc->isOpped($chan)){
+			$this->searchfor = $user;
+			$this->searchforchan = $chan;
+			$irc->who($user);
+		}
+	}
+	
+	public function kick(&$irc, $data, &$core){
+		if(substr($data->messageex[1],0,1)=="#"){$chan=$data->messageex[1];}else{$chan=$data->channel;}
+		if(!isset($data->messageex[1]) || (substr($data->messageex[1],0,1)=="#" && !isset($data->messageex[2]))){$user=$data->nick;}elseif(substr($data->messageex[1],0,1)=="#"){$user=$data->messageex[2];}else{$user=$data->messageex[1];}
+		if($irc->isOpped($chan)){
+			$irc->kick($chan, $user);
+		}
 	}
 	public function op(&$irc, $data, &$core){
 		if(substr($data->messageex[1],0,1)=="#"){$chan=$data->messageex[1];}else{$chan=$data->channel;}
+		if(!isset($data->messageex[1]) || (substr($data->messageex[1],0,1)=="#" && !isset($data->messageex[2]))){$user=$data->nick;}elseif(substr($data->messageex[1],0,1)=="#"){$user=$data->messageex[2];}else{$user=$data->messageex[1];}
 		if($irc->isOpped($chan)){
-			$irc->op($chan, "MRX");
+			$irc->op($chan, $user);
 		}
-		print_r($data);
+	}
+	
+	public function deop(&$irc, $data, &$core){
+		if(substr($data->messageex[1],0,1)=="#"){$chan=$data->messageex[1];}else{$chan=$data->channel;}
+		if(!isset($data->messageex[1]) || (substr($data->messageex[1],0,1)=="#" && !isset($data->messageex[2]))){$user=$data->nick;}elseif(substr($data->messageex[1],0,1)=="#"){$user=$data->messageex[2];}else{$user=$data->messageex[1];}
+		if($irc->isOpped($chan)){
+			$irc->deop($chan, $user);
+		}
+	}
+	
+	public function voice(&$irc, $data, &$core){
+		if(substr($data->messageex[1],0,1)=="#"){$chan=$data->messageex[1];}else{$chan=$data->channel;}
+		if(!isset($data->messageex[1]) || (substr($data->messageex[1],0,1)=="#" && !isset($data->messageex[2]))){$user=$data->nick;}elseif(substr($data->messageex[1],0,1)=="#"){$user=$data->messageex[2];}else{$user=$data->messageex[1];}
+		if($irc->isOpped($chan)){
+			$irc->voice($chan, $user);
+		}
+	}
+	
+	public function devoice(&$irc, $data, &$core){
+		if(substr($data->messageex[1],0,1)=="#"){$chan=$data->messageex[1];}else{$chan=$data->channel;}
+		if(!isset($data->messageex[1]) || (substr($data->messageex[1],0,1)=="#" && !isset($data->messageex[2]))){$user=$data->nick;}elseif(substr($data->messageex[1],0,1)=="#"){$user=$data->messageex[2];}else{$user=$data->messageex[1];}
+		if($irc->isOpped($chan)){
+			$irc->devoice($chan, $user);
+		}
 	}
 }
