0c2ca712aae1f9c186eb8636a83c1f3737b55ffe
Ramiro Bou <bouramiro@gmail.com>
2013-10-04 00:43:18 -0300

    añadiendo restart

--- modules/m_quit.php
+++ modules/m_quit.php
@@ -13,10 +13,14 @@ class pinkflyingelephants{
 	public function __construct(&$core){
 		$core->registerCommand("quit", "quit", "Desconecta del IRC y cierra el bot.", 10, "*", null, SMARTIRC_TYPE_QUERY|SMARTIRC_TYPE_CHANNEL);
 		$core->registerCommand("reconnect", "quit", "Desconecta del IRC y conecta de nuevo.", 10, "*", null, SMARTIRC_TYPE_QUERY|SMARTIRC_TYPE_CHANNEL);
-        
-        // TODO: Restart.
-	}
+		$core->registerCommand("restart", "quit", "Reinicia el bot.", 10, "*", null, SMARTIRC_TYPE_QUERY|SMARTIRC_TYPE_CHANNEL);
+   	}
 	
+	public function restart(&$irc, $data, &$core){
+		$irc->quit("[RESTART] Salida ordenada por un administrador.");
+		exec("php restart.php &");
+		exit;
+	}
 	public function quit(&$irc, $data, &$core){
         $irc->quit("[QUIT] Salida ordenada por un administrador.");
         die("Salida ordenada por un administrador");
