12d78dfce94340e95e25cd61354946f0db4d7cb0
Ramiro Bou <bouramiro@gmail.com>
2013-10-06 01:22:55 -0300

    Añadiendo humillación publica y medición de lag a otras personas

--- modules/m_ping.php
+++ modules/m_ping.php
@@ -22,13 +22,15 @@ class pinkflyingelephants{
 	public function ping(&$irc, $data, &$core){$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, $data->nick.': pong');}
 	public function pong(&$irc, $data, &$core){$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, $data->nick.': ping');}
 	public function pig(&$irc, $data, &$core){$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, $data->nick.': ¿Quieres un cerdo? ¡Ve a mirarte al espejo!');}
-	public function lag(&$irc, $data, &$core){$irc->message(SMARTIRC_TYPE_QUERY, $data->nick, "\001PING ".microtime(true)."\001");}
+	public function lag(&$irc, $data, &$core){
+		if(isset($data->messageex[1])){$u = $data->messageex[1];}else{$u=$data->nick;}
+		$irc->message(SMARTIRC_TYPE_CTCP_REQUEST, $u, "PING ".microtime(true));$this->lagchan=$data->channel;
+	}
     
     public function notha(&$irc, $data, &$core){
-        print_r($data);
         $rts = trim($data->messageex[1],"\001");
         $LAG = round(microtime(true) - $rts, 5) ;
-        $irc->message(SMARTIRC_TYPE_NOTICE, $data->nick, "Su lag es de $LAG segundos.");
+        $irc->message(SMARTIRC_TYPE_CHANNEL, $this->lagchan, "{$data->nick} tiene un lag de $LAG segundos.");
         
     }
     
