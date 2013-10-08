40b24fc1519abbb9115598d6da98b98a622ccc8f
Ramiro Bou <bouramiro@gmail.com>
2013-10-08 15:32:04 -0300

    Desde ahora los nombres de usuario serán almacenados en minúsculas en la base de datos PUEDE CAUSAR INCOMPATBILIDAD CON USUARIOS QUE CONTENGAN MAYUSCULAS

--- modules/m_authadd.php
+++ modules/m_authadd.php
@@ -32,7 +32,7 @@ class amodkey{
 			$irc->message(SMARTIRC_TYPE_QUERY, $data->nick, "Faltan parámetros. Sintaxis: register <usuario> <contraseña>");return 0;
 		}
 		$user = ORM::for_table('users')->create();
-		$user->username=$data->messageex[1];
+		$user->username=strtolower($data->messageex[1]);
 		$user->pass=sha1($data->messageex[2]);
 		$user->save();
 
@@ -46,7 +46,7 @@ class amodkey{
 	
 	public function listpriv(&$irc, &$data, &$core){
 		if(!isset($data->messageex[1])){$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "\00305Error:\003 Faltan parámetros!!");return 0;}
-		$user = ORM::for_table('users')->where('username', $data->messageex[1])->find_one();
+		$user = ORM::for_table('users')->where('username', strtolower($data->messageex[1]))->find_one();
 		$userpriv = ORM::for_table('userpriv')->where('uid', $user->id)->find_many();
 		$r="\002{$data->messageex[1]}\002 tiene los siguientes privilegios: ";
 		foreach($userpriv as $privuser){
@@ -59,7 +59,7 @@ class amodkey{
 	public function addpriv(&$irc, &$data, &$core){
 		if(!isset($data->messageex[3])){$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "\00305Error:\003 Faltan parámetros!!"); return 0;}
 		if($data->messageex[2]>9){$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "\00305Error:\003 Error de usuario. Inserte otro usuario y presione enter. (No se pueden otorgar privilegios de nivel 10!!)"); return 0;}
-		$user = ORM::for_table('users')->where('username', $data->messageex[1])->find_one(); 
+		$user = ORM::for_table('users')->where('username', strtolower($data->messageex[1]))->find_one(); 
 		$k = ORM::for_table('userpriv')->where('uid', $user->id)->where('sec',$data->messageex[3])->find_one();
 		if(method_exists($k, "delete")){$k->delete();}
 		$priv = ORM::for_table('userpriv')->create();
@@ -73,7 +73,7 @@ class amodkey{
 	public function delpriv(&$irc, &$data, &$core){
 		if(!isset($data->messageex[3])){$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "\00305Error:\003 Faltan parámetros!!");return 0;}
 		if($data->messageex[2]>9){$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "\00305Error:\003 Error de usuario. Inserte otro usuario y presione enter. (No se pueden otorgar privilegios de nivel 10!!)");}
-		$user = ORM::for_table('users')->where('username', $data->messageex[1])->find_one();
+		$user = ORM::for_table('users')->where('username', strtolower($data->messageex[1]))->find_one();
 		if(!isset($user->id)){return 0; } //el usuario no existeer 
 		$userpriv = ORM::for_table('userpriv')->where('uid', $user->id)->where("rng", $data->messageex[2])->where("sec", $data->messageex[3])->find_one();
 		if(method_exists($userpriv, "delete")){$userpriv->delete();}
