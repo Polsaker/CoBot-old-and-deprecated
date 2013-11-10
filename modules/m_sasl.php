5ae33c3c8f93b04ad4711b6cad80b4b1dbfa8463
Ramiro Bou <bouramiro@gmail.com>
2013-11-09 21:36:24 -0300

    Grrr... maldito SmartIRC

--- modules/m_sasl.php
+++ modules/m_sasl.php
@@ -12,12 +12,12 @@
 class imagenius{
 	public function __construct(&$core){
         $core->registerMessageHandler('AUTHENTICATE', "sasl", "reg");
-        $core->registerMessageHandler('AUTHENTICATE', "CAP", "cap");
-        $core->onconnect.="CAP REQ :sasl";
+  //      $core->registerMessageHandler('AUTHENTICATE', "CAP", "cap");
+        $core->onconnect.="CAP REQ :sasl\nAUTHENTICATE PLAIN";
 	}
 	
 	public function cap(&$irc, $data, $core){
-		$irc->send("AUTHENTICATE PLAIN", SMARTIRC_CRYTICAL);
+//		$irc->send("AUTHENTICATE PLAIN", SMARTIRC_CRYTICAL);
 	}
 	public function reg(&$irc, $data, $core){
 		$irc->send("AUTHENTICATE ".base64_encode("{$core->conf['nickserv']['nsuser']}\0{$core->conf['nickserv']['nsuser']}\0{$core->conf['nickserv']['nspass']}"), SMARTIRC_CRITICAL);
